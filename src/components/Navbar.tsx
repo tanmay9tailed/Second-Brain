a61f74726e15dc69cd314fa886eddf57d37c1471
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-12-05 20:23:32 +0530

    c

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar: React.FC<NavbarProps> = ({ setOpenSideBar, openSideBar, setAdded }
           },
         }
       );
-      setShareUrl(`https://second-brain-tanmay9taileds-projects.vercel.app/shared/${response.data.link}`);
+      setShareUrl(`https://second-brain-iq3h.onrender.com/shared/${response.data.link}`);
     } catch (error) {
       console.error("Error fetching share URL:", error);
       alert("Failed to fetch the share URL.");
