8259fc542c9c7b26dd79f29f2b14a21eaaa0dcc9
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-12-05 19:56:00 +0530

    c

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8888, // Use dynamic port or fallback to 5173
+    host: true, // Ensures it binds to 0.0.0.0 for external access
+  },
+});
